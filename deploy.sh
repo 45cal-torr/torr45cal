9d500710a348d29c053a5573a731e9e2746856dd
fourjiyog <123304710+fourjiyog@users.noreply.github.com>
2023-02-11 20:31:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-updated in stage branch to demonstrate the PR
+updated in stage branch to demonstrate the PR....
 updating this line in dev branch deploy file 
 Hello This file contains the deployment of sh file
